7f3fb36c4b4cc674ce45122b55e165a8c8a651d4
RodrigoBange <rodrigo99@live.nl>
2022-09-30 12:54:28 +0200

    Fixed pathing & renamed main form
    Fixed an issue where the program wouldn't load due to pathing not being able to be found.

---
+++ DemoApp/Main.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace DemoApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(18, 18);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(624, 344);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(668, 385);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Main";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DemoApp/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Logic;
+using Model;
+
+namespace DemoApp
+{
+    public partial class Main : Form
+    {
+        private Databases databases;
+        public Main()
+        {
+            InitializeComponent();
+            databases = new Databases();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            var dbList = databases.Get_All_Databases();
+            
+            foreach (var db in dbList)
+            {
+                listBox1.Items.Add(db.name);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
