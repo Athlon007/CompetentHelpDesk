6f05ed34d604498df3127d6e265da5a4bc6bbe14
Athlon007 <aathlon@outlook.com>
2022-10-17 22:24:22 +0200

    Update StatusStruct.cs

--- Logic/StatusStruct.cs
+++ Logic/StatusStruct.cs
@@ -2,19 +2,37 @@
 {
     public struct StatusStruct
     {
-        public int Code { get; private set; } // 0 = OK. 1 == Exception.
+        /// <summary>
+        /// Status code of the operation.<br></br><br></br>
+        /// <b>0</b> = OK.<br></br>
+        /// <b>1</b> (and above) = Fail
+        /// </summary>
+        public int Code { get; private set; }
+
+        /// <summary>
+        /// FRIENDLY (to the user) error message displayed in the GUI, if applicable.
+        /// </summary>
         public string Message { get; private set; }
 
+        /// <summary>
+        /// Create new Status response without a message.
+        /// </summary>
+        /// <param name="code">Error code. 0 = OK.</param>
         public StatusStruct(int code)
         {
             Code = code;
             Message = "";
         }
 
+        /// <summary>
+        /// Create new Status response with a message.
+        /// </summary>
+        /// <param name="code">Error code. 0 = OK.</param>
+        /// <param name="message">Friendly message displayed in GUI.</param>
         public StatusStruct(int code, string message)
         {
-            this.Code = code; 
-            this.Message = message;
+            Code = code; 
+            Message = message;
         }
     }
 }
