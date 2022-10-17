b03ad7bb605e3c426a9f7eb7acd1bb8668c9935c
Athlon007 <aathlon@outlook.com>
2022-10-17 15:24:50 +0200

    Add Unit Tests for Ticket Escalator

---
+++ TestDbConnectionNew/TestEscalation.cs
@@ -0,0 +1,35 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+
+namespace TestDbConnectionNew
+{
+    [TestClass]
+    public class TestEscalation
+    {
+        private TicketEscalationService service;
+
+        public TestEscalation()
+        {
+            service = new TicketEscalationService();
+        }
+
+        [TestMethod]
+        public void TestTicketEscalatableTrue()
+        {
+            Ticket ticket = new Ticket();
+            ticket.EscalationLevel = 0;
+            Assert.AreEqual(true, service.IsTicketEscalatable(ticket));
+        }
+
+        [TestMethod]
+        public void TestTicketEscalatableFalse()
+        {
+            int maxLevels = Enum.GetValues(typeof(EmployeeType)).Length;
+            Ticket ticket = new Ticket();
+            ticket.EscalationLevel = maxLevels;
+            Assert.AreEqual(false, service.IsTicketEscalatable(ticket));
+        }
+    }
+}
