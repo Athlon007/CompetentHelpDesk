1dc0c9514b73d947b545d4d868ea1121be122ca0
MariaRaluca Diaconescu <MRDiaconescu@outlook.com>
2022-10-25 10:48:21 +0200

    Submitted LoginService

--- DemoApp/LoginScreen.Designer.cs
+++ DemoApp/LoginScreen.Designer.cs
@@ -210,7 +210,6 @@
             this.btnSendResetLink.TabIndex = 7;
             this.btnSendResetLink.Text = "Reset";
             this.btnSendResetLink.UseVisualStyleBackColor = false;
-            this.btnSendResetLink.Click += new System.EventHandler(this.btnSendResetLink_Click);
             // 
             // txtEmail
             // 
